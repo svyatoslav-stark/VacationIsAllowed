6425c0839f39e57a9163b8b00c40ddd3e1b5833e
Svyatoslav Starkov <svyatoslav.starkov@yandex.ru>
2025-03-12 20:51:16 +0500

    Changes VacationServiceTest.java

--- src/test/java/VacationServiceTest.java
+++ src/test/java/VacationServiceTest.java
@@ -1,26 +1,16 @@
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import ru.netology.services.VacationService;
 
 public class VacationServiceTest {
 
-    @Test
-    void testFirstScenario() {
+    @ParameterizedTest
+    @CsvFileSource(files="src/test/resources/Vacation.csv")
+    public void testVacationMonthsVacation(int expected, int income, int expenses, int threshold) {
         VacationService service = new VacationService();
 
-        int expected = 3;
-        int actual = service.calculate(10_000, 3_000, 20_000);
-        
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    void testSecondScenario() {
-        VacationService service = new VacationService();
-
-        int expected = 2;
-        int actual = service.calculate(100_000, 60_000, 150_000);
+        int actual = service.calculate(income, expenses, threshold);
 
         Assertions.assertEquals(expected, actual);
     }
